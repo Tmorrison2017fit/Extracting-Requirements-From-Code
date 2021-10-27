25a14ee9c7d8fb33dbd30983636d3aa039cec514
Ivan Hernandez <ihernandez2018@my.fit.edu>
2021-10-27 18:13:50 -0400

    Example prints a while loop

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,23 @@ int main(int argc,char* argv[]){
   // Creates a Method object.
   File::Method obj;
   // Calls a function within the Method Class.
-  obj.PutName("Test Name");
+  obj.PutName("main");
   // Adds the Method obj to the File Class.
   TestFile->AddMethod(obj);
   // Example on how to access the method within the File Class.
-  cout << TestFile->List[0].GetName() << endl;
+  cout << TestFile->MethodsInFile[0].GetName() << endl;
   // Not final, just used to test data storage
   Parse_File(argv[1], TestFile);
 
   // Prints the stored data.
-  int size = TestFile->List[0].GetLength();
-  for( int i =0; i<size; i++){
-    cout << TestFile->List[0].GetLine(i) << endl;
+  TestFile->MethodsInFile[0].AddLoopRange(15,20);
+  vector<int> Len = TestFile->MethodsInFile[0].GetLoopRange(0);
+
+  for( int i = Len[0]; i <= Len[1]; i++){
+    cout << TestFile->MethodsInFile[0].GetLine(i) << endl;
   }
+
+
 
   return 0;
 }
